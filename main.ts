9a3aa6f0140c7a7975d9663dd4b5ce4d52b29099
Jayden Pyles <jpylesbusiness@gmail.com>
2024-01-12 20:45:11 -0600

    init commit

--- main.ts
+++ main.ts
@@ -1,134 +1,87 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin, Notice, Menu } from "obsidian";
+import { exec } from "child_process";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+enum BookstackCommands {
+	push = "remote",
+	pull = "local",
+	updateRemote = "update --remote",
+	updateLocal = "update --local",
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+export default class ObsidianToBookstackPlugin extends Plugin {
+	private doppler: boolean;
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	onload() {
+		const pluginPath = this.app.vault.adapter.basePath; //type: ignore
+		process.chdir(pluginPath);
 
-	async onload() {
-		await this.loadSettings();
+		this.checkIfDoppler();
+		this.addRibbonIcon("book", "Bookstack", (event) => {
+			const menu = new Menu();
+			menu.addItem((item) =>
+				item
+					.setTitle("Push to Bookstack")
+					.setIcon("book-up")
+					.onClick(() =>
+						this.runBookstackCommands([
+							BookstackCommands.push,
+							BookstackCommands.updateRemote,
+						])
+					)
+			);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			menu.addItem((item) =>
+				item
+					.setTitle("Pull from Bookstack")
+					.setIcon("book-down")
+					.onClick(() =>
+						this.runBookstackCommands([
+							BookstackCommands.pull,
+							BookstackCommands.updateLocal,
+						])
+					)
+			);
+			menu.showAtMouseEvent(event);
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	}
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	checkIfDoppler() {
+		const command = "doppler --version";
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		exec(command, (error, stdout, stderr) => {
+			if (error || stderr) {
+				new Notice(`Doppler not installed or not properly set.`);
+				this.doppler = false;
+				return;
+			} else {
+				this.doppler = true;
 			}
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	buildBaseCommand(): string {
+		let command = "obsidian_to_bookstack";
+
+		if (this.doppler) {
+			command = "doppler run -- " + command;
+		}
+
+		return command;
+	}
+
+	runBookstackCommands(args: Array<BookstackCommands>) {
+		args.forEach((arg) => {
+			let command = this.buildBaseCommand();
+			command = `${command} ${arg}`;
+
+			exec(command, (error, stdout, stderr) => {
+				if (error || stderr) {
+					new Notice(`Error: ${error || stderr}`);
+					console.log(`${error}`);
+					return;
 				}
-			}
+				new Notice(`Output: ${stdout}`);
+			});
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
