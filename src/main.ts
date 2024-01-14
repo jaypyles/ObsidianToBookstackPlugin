46a0c71ce4a38eb16473e6aff57c4f14931e706e
Jayden Pyles <jpylesbusiness@gmail.com>
2024-01-13 19:40:19 -0600

    feat: add settings menu for configuration options

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Plugin, Notice, Menu } from "obsidian";
+import { App, Setting, Plugin, Notice, Menu, PluginSettingTab } from "obsidian";
 import { exec } from "child_process";
 
 enum BookstackCommands {
@@ -24,10 +24,22 @@ const logMessages: LogMessages = {
 	[BookstackCommands.updateLocal]: "Updating local files",
 };
 
+interface ObsidianToBookstackSettings {
+	configPath: string;
+	envPath: string;
+}
+
+const DEFAULT_SETTINGS: ObsidianToBookstackSettings = {
+	configPath: "~/.config/obsidian_to_bookstack/conf.toml",
+	envPath: "",
+};
+
 export default class ObsidianToBookstackPlugin extends Plugin {
 	private doppler: boolean;
+	settings: ObsidianToBookstackSettings;
 
-	onload() {
+	async onload() {
+		await this.loadSettings();
 		// @ts-ignore
 		const pluginPath = this.app.vault.adapter.basePath;
 		process.chdir(pluginPath);
@@ -60,6 +72,22 @@ export default class ObsidianToBookstackPlugin extends Plugin {
 			);
 			menu.showAtMouseEvent(event);
 		});
+
+		this.addSettingTab(
+			new ObsidianToBookstackPluginSettingTab(this.app, this)
+		);
+	}
+
+	async loadSettings() {
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
 	}
 
 	checkIfDoppler() {
@@ -79,6 +107,14 @@ export default class ObsidianToBookstackPlugin extends Plugin {
 	buildBaseCommand(): string {
 		let command = "obsidian_to_bookstack";
 
+		if (this.settings.configPath) {
+			command = `${command} --config ${this.settings.configPath}`;
+		}
+
+		if (this.settings.envPath) {
+			command = `${command} --env ${this.settings.envPath}`;
+		}
+
 		if (this.doppler) {
 			command = "doppler run -- " + command;
 		}
@@ -90,6 +126,7 @@ export default class ObsidianToBookstackPlugin extends Plugin {
 		args.forEach((arg) => {
 			let command = this.buildBaseCommand();
 			command = `${command} ${arg}`;
+			console.log(command);
 
 			exec(command, (error, stdout, stderr) => {
 				if (error || stderr) {
@@ -100,5 +137,46 @@ export default class ObsidianToBookstackPlugin extends Plugin {
 				new Notice(logMessages[arg]);
 			});
 		});
+	}
+}
+
+class ObsidianToBookstackPluginSettingTab extends PluginSettingTab {
+	plugin: ObsidianToBookstackPlugin;
+	constructor(app: App, plugin: ObsidianToBookstackPlugin) {
+		super(app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		const { containerEl } = this;
+		containerEl.empty();
+
+		new Setting(containerEl)
+			.setName("Configuration File Path")
+			.setDesc(
+				"Configuration file to load for use with `obsidian-to-bookstack`"
+			)
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter the path to your config.")
+					.setValue(this.plugin.settings.configPath)
+					.onChange(async (value) => {
+						this.plugin.settings.configPath = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Env File Path")
+			.setDesc("Env file to load for use with `obsidian-to-bookstack`")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter the path to your env file.")
+					.setValue(this.plugin.settings.envPath)
+					.onChange(async (value) => {
+						this.plugin.settings.envPath = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
